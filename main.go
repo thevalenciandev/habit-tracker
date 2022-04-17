ea3b644d934917f43ec3b80529c5165dba4d6a66
thevalenciandev <thevalenciandev@gmail.com>
2022-04-17 17:36:25 +0100

    Refactor: better error handling

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"regexp"
@@ -17,6 +18,17 @@ type Habit struct {
 	Days []string `json:"days"`
 }
 
+// Custom Handler that can return errors
+type appHandler func(w http.ResponseWriter, r *http.Request) error
+
+// Our custom Handler implements the http.Handler interface and wraps
+// it with error handling capabilities
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := fn(w, r); err != nil {
+		http.Error(w, "Unexpected error", http.StatusInternalServerError) //TODO should we give more details of the error?
+	}
+}
+
 var (
 	habitIdRegexp = regexp.MustCompile(`/v1/habits/([0-9]+)$`)
 	habits        = make(map[int]Habit)
@@ -26,58 +38,53 @@ var (
 func main() {
 	habits[habitCnt] = Habit{Id: "1", Name: "Play Guitar 🎸", Days: []string{"Monday", "Sunday"}}
 
-	http.HandleFunc("/v1/habits", allHabitsHandler)
-	http.HandleFunc("/v1/habits/", habitByIdHandler)
+	http.Handle("/v1/habits", appHandler(allHabitsHandler))
+	http.Handle("/v1/habits/", appHandler(habitByIdHandler))
 
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("Started HabitTracker server on 8080.")
 }
 
-func habitByIdHandler(w http.ResponseWriter, r *http.Request) {
+func habitByIdHandler(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
 	case "GET":
 		matches := habitIdRegexp.FindStringSubmatch(r.URL.Path)
-		if matches != nil && len(matches) == 2 {
-			id, err := strconv.Atoi(matches[1])
-			if err == nil {
-				fmt.Println("Id requested", id)
-				h, ok := habits[id]
-				if ok {
-					encodeAsJson(h, w)
-					return
-				} else {
-					http.Error(w, "Not found", http.StatusNotFound)
-					return
-				}
-			}
+		if matches != nil || len(matches) != 2 {
+			return errors.New("Path " + r.URL.Path + " not found")
 		}
+		id, err := strconv.Atoi(matches[1])
+		if err != nil {
+			return err
+		}
+		h, ok := habits[id]
+		if !ok {
+			return errors.New("Habit " + strconv.Itoa(id) + " not found")
+		} else {
+			return encodeAsJson(h, w)
+		}
+	default:
+		return errors.New("Method " + r.Method + " not supported")
 	}
-	http.Error(w, "Unexpected error", http.StatusInternalServerError) //TODO should we give more details of the error?
 }
 
 func encodeAsJson(toEncode any, w http.ResponseWriter) error {
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(toEncode)
-	if err != nil {
-		http.Error(w, "Unexpected error", http.StatusInternalServerError)
-	}
-	return err
+	return json.NewEncoder(w).Encode(toEncode)
 }
 
-func allHabitsHandler(w http.ResponseWriter, r *http.Request) {
+func allHabitsHandler(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
 	case "GET":
-		encodeAsJson(maps.Values(habits), w)
+		return encodeAsJson(maps.Values(habits), w)
 	case "POST":
 		var h Habit
 		if err := json.NewDecoder(r.Body).Decode(&h); err != nil {
-			http.Error(w, "Unexpected error decoding habit", http.StatusNoContent)
-			return
+			return err
 		}
 		habitCnt++
 		habits[habitCnt] = h
-		encodeAsJson(h, w)
+		return encodeAsJson(h, w)
 	default:
-		http.Error(w, "Method not supported ", http.StatusMethodNotAllowed)
+		return errors.New("Method " + r.Method + " not supported")
 	}
 }
